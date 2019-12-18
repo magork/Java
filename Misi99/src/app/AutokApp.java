7aceb2ae296456382a101fecb0d341fca928bd44
magork <magork87@gmail.com>
2019-12-18 18:14:24 +0200

    komment

--- Misi99/src/app/AutokApp.java
+++ Misi99/src/app/AutokApp.java
@@ -3,7 +3,7 @@ package app;
 public class AutokApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub Misi99 applikció
 
 	}
 
