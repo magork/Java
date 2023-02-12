83573a3341ec09e9d55237309653dfb391f20138
magork <magork87@gmail.com>
2023-02-12 07:19:28 +0000

    vsc modification

--- Misi99/src/app/AutokApp.java
+++ Misi99/src/app/AutokApp.java
@@ -1,9 +1,19 @@
 package app;
+import java.util.Scanner;
 
 public class AutokApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub Misi99 applikció
+		Scanner sc=new Scanner(System.in);
+		System.out.println("================================");
+		for(int i=0;i<3;i++){
+			String s1=sc.next();
+			int x=sc.nextInt();
+			//Complete this line
+		}
+		System.out.println("================================");
+
 
 	}
 
